b025d13a42d0790b52feaa05808a563c82052f81
Bruno Baltazar <bruno.alves.baltazar@gmail.com>
2020-01-26 18:58:24 +0000

    att script

--- deployer.sh
+++ deployer.sh
@@ -1,14 +1,15 @@
-#!/bin/sh -ex
+#!/bin/sh
+
+
 export PATH=$PATH:/usr/bin/ > /dev/null 2>&1
-echo $USER
-docker -v
+
 # BRANCH_TAG=$1
 # SISTEMA=$2
 # PORT=$3
-# case $P_APP_NAME in #(executar script e passar paramentro <nome_aplicação> como opção)
-# export BRANCH_TAG=$(echo $BRANCH_TAG | cut -d'/' -f 2);
 
+# export BRANCH_TAG=$(echo $BRANCH_TAG | cut -d'/' -f 2);
 #export HOST=$(cat /etc/hostname);
+
 #export BRANCH_TAG=master
 #export SISTEMA=php.nginx-dockerize
 #export PORT=8000 #"${PORT}"
@@ -17,8 +18,7 @@ docker -v
 #echo "-------------------------------------------------------------------------------"
 #echo "   Clonando  projeto ${SISTEMA} do git  "${BRANCH_TAG}"                        "
 #echo "-------------------------------------------------------------------------------"
-#echo ${APP_PATH_DEPLOY}
-#echo ${SISTEMA}
+
 
 #if [ -d "$APP_PATH_DEPLOY" ]
 #then
@@ -29,8 +29,9 @@ docker -v
 # Necessário add gera um chave publica na sua maquina e depois adicionar no servidor gitlab
 # Usuário deve ser dono da pasta vagrant ou ter permissão de leitura e escrita
 #echo "O diretório não existe iniciando a clonagem do projeto";
-#   git clone -b ${BRANCH_TAG} ssh://git@git.tj.ce.gov.br/sistemas/${SISTEMA}.git /opt/vagrant/${SISTEMA}   
+#   git clone -b ${BRANCH_TAG} ssh://git@github.com:brunobaltazar/${SISTEMAS}.git /opt/vagrant/${SISTEMA}
 #fi
+
 
 #cd .. && pwd && ls -l && docker-compose up -d --build
 
