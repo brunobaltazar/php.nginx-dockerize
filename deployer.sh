ab992e6fb2942a6a7e231ec6df18a50a21d4cb6a
Bruno Baltazar <bruno.alves.baltazar@gmail.com>
2020-01-26 10:46:23 -0300

    Update deployer.sh

--- deployer.sh
+++ deployer.sh
@@ -1,3 +1,6 @@
-#!/bin/sh -e
-export BRANCH=$(echo $BRANCH_TAG | cut -d'/' -f 2);
-export HOST_SERVER=
+USERNAME=$1
+HOSTS="$2 #host2 #host3"
+SCRIPT="pwd; ls"
+for HOSTNAME in ${HOSTS} ; do
+    ssh -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
+done
